459b2f534bbc44fb8a574f79561217599f289232
Hashim Gari <hashmaster3k@gmail.com>
2020-07-01 13:36:53 -0600

    Finished iteration 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -8,5 +8,13 @@ class Restaurant
     @dishes = []
   end
 
+  def closing_time(time)
+    (@opening_time.to_i + time).to_s + ":00"
+  end
+
+  def add_dish (dish)
+    @dishes << dish
+  end
+
 
 end
