c9a1c208b9e444ca956871612dc53b05b75d5a4e
Hashim Gari <hashmaster3k@gmail.com>
2020-07-01 13:23:07 -0600

    Add opening time method in restaurant.rb

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,8 +1,10 @@
 class Restaurant
 
-def initialize (opening_time, restaurant_name)
-  @opening_time = opening_time
-  @restaurant_name = restaurant_name
-end
+  attr_reader :opening_time, :restaurant_name
+
+  def initialize (opening_time, restaurant_name)
+    @opening_time = opening_time
+    @restaurant_name = restaurant_name
+  end
 
 end
